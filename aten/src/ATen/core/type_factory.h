60c31e949ef95ca7d0a83f909ba15d24cd028b7b
zhxchen17 <zhxchen17@fb.com>
2022-01-21 18:47:57 -0800

    [jit][edge] Migrate to TypeFactory for jit types in schema type parser.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71516
    
    Function schema should be able to contruct dynamic types when on mobile.
    ghstack-source-id: 147465694
    
    Differential Revision: [D33673958](https://our.internmc.facebook.com/intern/diff/D33673958/)

--- aten/src/ATen/core/type_factory.h
+++ aten/src/ATen/core/type_factory.h
@@ -1,12 +1,18 @@
 #pragma once
 
+#include <type_traits>
+
 #include <ATen/core/dynamic_type.h>
 #include <ATen/core/jit_type.h>
-#include <type_traits>
+#include <c10/macros/Macros.h>
 
 namespace c10 {
 
-struct TORCH_API DynamicTypeFactory {
+template <typename T>
+struct TORCH_API TypeFactoryBase {};
+
+template <>
+struct TORCH_API TypeFactoryBase<c10::DynamicType> {
   template <typename T, typename... Args>
   static c10::DynamicTypePtr create(TypePtr ty, Args&&... args) {
     return std::make_shared<c10::DynamicType>(
@@ -29,26 +35,40 @@ struct TORCH_API DynamicTypeFactory {
         name,
         c10::DynamicType::Arguments(fields, types));
   }
+  template <typename T>
+  C10_ERASE static c10::DynamicTypePtr createNamed(const std::string& name) {
+    return std::make_shared<c10::DynamicType>(
+        c10::DynamicTypeTrait<T>::tagValue(),
+        name,
+        c10::DynamicType::Arguments{});
+  }
+  template <typename T>
+  C10_ERASE static c10::DynamicTypePtr get() {
+    return DynamicTypeTrait<T>::getBaseType();
+  }
   static const std::unordered_map<std::string, c10::TypePtr>& basePythonTypes();
 };
+
+using DynamicTypeFactory = TypeFactoryBase<c10::DynamicType>;
 
 // Helper functions for constructing DynamicTypes inline.
 template <
     typename T,
     std::enable_if_t<DynamicTypeTrait<T>::isBaseType, int> = 0>
-DynamicTypePtr dynT() {
-  return DynamicTypeTrait<T>::getBaseType();
+C10_ERASE DynamicTypePtr dynT() {
+  return DynamicTypeFactory::get<T>();
 }
 
 template <
     typename T,
     typename... Args,
     std::enable_if_t<!DynamicTypeTrait<T>::isBaseType, int> = 0>
-DynamicTypePtr dynT(Args&&... args) {
+C10_ERASE DynamicTypePtr dynT(Args&&... args) {
   return DynamicTypeFactory::create<T>(std::forward<Args>(args)...);
 }
 
-struct TORCH_API DefaultTypeFactory {
+template <>
+struct TORCH_API TypeFactoryBase<c10::Type> {
   template <typename T, typename... Args>
   static c10::TypePtr create(TypePtr ty, Args&&... args) {
     return T::create(std::move(ty), std::forward<Args>(args)...);
@@ -63,15 +83,27 @@ struct TORCH_API DefaultTypeFactory {
       const std::vector<c10::TypePtr>& types) {
     return c10::TupleType::createNamed(name, fields, types);
   }
+  template <typename T>
+  C10_ERASE static c10::TypePtr createNamed(const std::string& name) {
+    return T::create(name);
+  }
   static const std::unordered_map<std::string, c10::TypePtr>& basePythonTypes();
+  template <typename T>
+  C10_ERASE static c10::TypePtr get() {
+    return T::get();
+  }
 };
 
-using TypeFactory =
+using DefaultTypeFactory = TypeFactoryBase<c10::Type>;
+
+using PlatformType =
 #ifdef C10_MOBILE
-    DynamicTypeFactory
+    c10::DynamicType
 #else
-    DefaultTypeFactory
+    c10::Type
 #endif
     ;
+
+using TypeFactory = TypeFactoryBase<PlatformType>;
 
 } // namespace c10
